67b4e7f0b237b65912b53c94b05c65e33b2fa1ad
Jtansjevhscscc <135506555+Jtansjevhscscc@users.noreply.github.com>
2023-08-17 14:45:29 +0200

    Delete connection.php

--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Récupérer les données du formulaire
-$email = $_POST['email'];
-$password = $_POST['password'];
-
-// Traitement des données (ici, affichage à des fins de démonstration)
-echo "Adresse e-mail : " . $email . "<br>";
-echo "Mot de passe : " . $password;
-?>
